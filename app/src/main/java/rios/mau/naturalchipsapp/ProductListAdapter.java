55aeaff23548c81201a91cedbd01a6701028de04
Mauricio Rios <mauri.96.rv@gmail.com>
2018-12-22 12:27:32 -0400

    sql done

---
+++ app/src/main/java/rios/mau/naturalchipsapp/ProductListAdapter.java
@@ -0,0 +1,78 @@
+package rios.mau.naturalchipsapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class ProductListAdapter extends BaseAdapter {
+
+    private Context context;
+    private  int layout;
+    private ArrayList<Product> productList;
+
+    public ProductListAdapter(Context context, int layout, ArrayList<Product> prooductList) {
+        this.context = context;
+        this.layout = layout;
+        this.productList = prooductList;
+    }
+
+    @Override
+    public int getCount() {
+        return productList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return productList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder{
+        ImageView imageView;
+        TextView txtName, txtPrice;
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup viewGroup) {
+
+        View row = view;
+        ViewHolder holder = new ViewHolder();
+
+        if(row == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(layout, null);
+
+            holder.txtName = (TextView) row.findViewById(R.id.txtName);
+            holder.txtPrice = (TextView) row.findViewById(R.id.txtPrice);
+            holder.imageView = (ImageView) row.findViewById(R.id.imgProduct);
+            row.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) row.getTag();
+        }
+
+        Product product = productList.get(position);
+
+        holder.txtName.setText(product.getName());
+        holder.txtPrice.setText(product.getPrice());
+
+        byte[] productImage = product.getImage();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(productImage, 0, productImage.length);
+        holder.imageView.setImageBitmap(bitmap);
+
+        return row;
+    }
+}
